e16fec22659d9a3e4c40f6aef4de9e7e55bc5557
Gehad Makhlouf <gehadmakhlouf@gmail.com>
2019-02-17 11:37:08 +0200

    Exercise-Chapter-Two-2.2

---
+++ Exercise-2.2.py
@@ -0,0 +1,8 @@
+#Exercise 2: Write a program that uses input to prompt a user for their
+#name and then welcomes them.
+#Chuck
+#Hello Chuck
+#print ("Enter your name:")
+
+name = raw_input ("Enter your name:")
+print ("Hello "+ name)
